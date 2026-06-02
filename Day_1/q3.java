b7a1c1aff208b368c4b17daaa88c95d44b8b0012
Kartikey Rai <25114cn375@glbitm.ac.in>
2026-06-02 11:56:57 +0530

    Add files via upload

---
+++ Day_1/q3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        
+        if (num < 0) {
+            System.out.println("Error: Factorial is not defined for negative numbers.");
+        } else {
+            long result = 1; 
+            
+           
+            for (int i = 1; i <= num; i++) {
+                result *= i;
+            }
+            
+            System.out.println("The factorial of " + num + " is: " + result);
+        }
+        
+        
+    }
+}
